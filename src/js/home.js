04232172de606cb25b14c76093d225eccf995e5f
Adhamjon Erkinov <adhamjonerkinov65@gmail.com>
2022-12-25 16:21:55 +0500

    praduct page

--- src/js/home.js
+++ src/js/home.js
@@ -3,13 +3,13 @@ export function displayProducts(data = []) {
   let result = "";
   const productDetails = document.querySelector(".crew__product");
   data.forEach((prudoct) => {
-    let { img, name ,description, salePrice} = prudoct;
+    let {id, img, name ,description, salePrice, quantity} = prudoct;
     const imgs = img
       ? img
       : configs.defaultImg + "400";
     result += `
 
-            <div class="col">
+            <div class="col" data-id="${id}>
                 <article class="card">
                     <div class="card__header">
                       <img class="card__img" width="100%" src="${imgs}" alt="img">
@@ -17,11 +17,13 @@ export function displayProducts(data = []) {
                     <div class="card__body">
                       <div class="card__title">${name}</div>
                       <div class="card__discriptiop">${description}</div>
-                      <div class="card__salePrice">${salePrice}</div>
-    
-        
+                      <div class="row">
+                      <div class="col card__salePrice">${salePrice}</div>
+                      <div class="card__salePrice">${quantity}</div>
+                      </div>
                     </div>
                  </article>
+
             </div>`;
   });
   productDetails.innerHTML = result;
